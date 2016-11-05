c3485b21ec311a3829397d7107ed104294162a94
Wolfgang Halbwirth <w.halbwirth@gmail.com>
2016-11-05 12:01:30 +0100

    wh

---
+++ NTM/WebContent/lauflaengen.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/stylesheet.css">
+    <title>NTM_Huffman</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  
+		<div class="jumbotron text-left" onclick="window.location.href='index.html'">
+			<div class="logo" ><img src="img/logo.png" alt="logo" height="200px" width="200px"></div>
+		  	<h1>Netzwerktechnologie für Multimedia Anwendungen UE - Gruppe 4</h1>
+		</div>
+		
+		
+		<h1>Hier kommt der Lauflängencode</h1>
+
+
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
